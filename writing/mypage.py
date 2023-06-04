d177c338fb176e2058bed3360fb6fd161e3fd064
leehjhjhj <tsukiakarii@naver.com>
2023-06-04 21:37:20 +0900

    fix: 도장 수정

--- writing/mypage.py
+++ writing/mypage.py
@@ -147,6 +147,9 @@ class WhatILikeView(ListAPIView):
   
 
 class WeekIsWritingView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def get(self, *args, **kwargs):
         today = datetime.today()
         start_of_week = today - timedelta(days=today.weekday())
@@ -155,12 +158,9 @@ class WeekIsWritingView(APIView):
         result = {str((start_of_week + timedelta(days=i)).date()): 0 for i in range(7)}
         
         post_dates = Post.objects.filter(user_id=user.id, created_at__range=[start_of_week.date(), end_of_week.date() + timedelta(days=1)]).only('created_at')
-        
         if post_dates.exists():
             for post in post_dates:
                 date = str(post.created_at.date())
                 result[date] = 1
-        else:
-            return Response({'week_is_writing': result}, status=status.HTTP_200_OK)
         
         return Response({'week_is_writing': list(result.values())}, status=status.HTTP_200_OK)
