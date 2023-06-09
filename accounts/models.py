7b89102d1e17b7852bdebcc0aa8d579169005630
leehjhjhj <tsukiakarii@naver.com>
2023-06-09 16:40:28 +0900

    feat: feedback admin 추가

--- accounts/models.py
+++ accounts/models.py
@@ -16,4 +16,5 @@ class User(AbstractUser):
 
 class Feedback(models.Model):
      body = models.CharField(max_length=200)
+     created_at = models.DateTimeField(auto_now_add=True, null=True)
      
